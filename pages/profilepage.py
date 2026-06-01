effa6de34a19b013c2126e204b2924434ca5a04f
SUNEHA KUMARI <sunehasahu04@gmail.com>
2026-06-01 22:00:48 +0530

    Add files via upload

---
+++ pages/profilepage.py
@@ -0,0 +1,86 @@
+
+
+import streamlit as st
+import pymongo
+from PIL import Image
+conn=pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.3")
+mydb=conn["experi"]
+my=mydb["supriya"]
+
+@st.dialog("CHANGE PASSWORD")
+
+def cp():
+       t1=st.text_input("Enter The Old Password")
+       t2=st.text_input("Enter The New Password")
+       if st.button("Change Password"):
+              res=my.update_one({"password":t1},{'$set':{"password":t2}})
+              st.success("Password Changed Successfully")
+
+
+
+c1,c2,c3,c6=st.columns(4)
+c4,c5=st.columns(2)
+c4.header("cv analysis..")
+if c1.button("Change Password",use_container_width=True):
+           cp()
+if c2.button("See profile",use_container_width=True):
+           str1=st.session_state['username']
+           str2=st.session_state['password']
+           res=my.find({"Username":srt1,"password":str2})
+           st.success("USER PROFILE")
+           for data in res:
+                   st.text_input("Username",data["username"])
+                   st.text_input("Password",data["password"]) 
+                   st.text_input("Address",data["address"]) 
+                   st.text_input("Course",data["course"])
+                   st.text_input("DOB",data["dob"]) 
+                   st.text_input("Mobile No",data["mobileno"])   
+
+b3=c3.button("CV Analysis",use_container_width=True)
+if c6.button("Logout",use_container_width=True):
+              del st.session_state["username"]
+              del st.session_state["password"]
+              st.switch_page("main.py")
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
