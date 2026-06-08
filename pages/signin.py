93b101c817cf65f154d25a56914c7973611a97c6
SUNEHA KUMARI <sunehasahu04@gmail.com>
2026-06-08 19:08:25 +0530

    Update signin.py

--- pages/signin.py
+++ pages/signin.py
@@ -10,7 +10,7 @@ st.subheader("SignIn")
 
 with st.form("SignIn"):
        t1=st.text_input("👤Username")
-       t2=st.text_input("🔒 Password")
+       t2=st.text_input("🔒 Password",type="password)
        if st.form_submit_button("SignIn"):
               if not t1 or not t2:
                      st.error("Fill The Fields!!!")
