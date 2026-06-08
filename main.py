1a201928b9107ae7d61f27ed2f29e04f4c78434c
SUNEHA KUMARI <sunehasahu04@gmail.com>
2026-06-08 13:18:08 +0530

    Fix formatting issues in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time
 st.title("WELCOME IN PYTHON WEB WORLD!!!")
-p=st.progress(0,"Wait for Loading ....")
+p=st.progress(0,"Wait for Loading ...")
 for i in range(1,100):
        time.sleep(0.05)
        p.progress(i,"Loading....")
