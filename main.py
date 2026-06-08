af9db40628bd017617726b30cfa3ad98e9109cb1
SUNEHA KUMARI <sunehasahu04@gmail.com>
2026-06-08 13:19:04 +0530

    Fix title duplication and formatting in main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ for i in range(1,100):
        p.progress(i,"Loading....")
 p.empty()
 st.image("p4.jpeg",width=5000)
-st.subheader("WELCOME IN PYTHON WEB WORLD!!! bgtyihfr vg6iujg fr78jjhh
+st.title("WELCOME IN PYTHON WEB WORLD!!! bgtyihfr vg6iujg fr78jjhh
 ggreryjhjh hftujfr vftilkkjg gtykjngft gfyukjjftukj dxdrtyhggty hyujhhhgjk gryjjgyu ngfytukmjh fyum")
 
 
